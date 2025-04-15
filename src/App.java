db54db9f5eb42891fc312a1650eeb84cad754f85
stfx <mustaphabennasser8@gmail.com>
2025-04-15 01:31:46 +0100

    dd

---
+++ src/App.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class App {
+    public static void main(String[] args) {
+        String url = "jdbc:mariadb://localhost:3306/buvette";
+        String user = "stof";
+        String password = "bennasser";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println("✅ Connected to MariaDB!");
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println("❌ Connection failed!");
+            e.printStackTrace();
+        }
+    }
+}
